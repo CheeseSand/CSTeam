c5db939660ffab574958d19a321e20e7bfc9ea5a
CheeseSand <choijoung1479@gmail.com>
2025-02-12 21:12:34 +0900

    권한 체크 추가

--- src/main/kotlin/org/cheesesand/csTeam/TeamException.kt
+++ src/main/kotlin/org/cheesesand/csTeam/TeamException.kt
@@ -5,5 +5,11 @@ import net.kyori.adventure.text.format.TextColor
 
 class TeamException(val component: Component) : Throwable()
 
+val isNotAPlayer = TeamException(Component.text("해당 명령어는 플레이어가 아니면 실행할 수 없습니다")
+    .color(TextColor.fromHexString("#FF5555")))
+
 val executedCommandIsNotCorrect = TeamException(Component.text("실행할 명령어가 올바르지 않습니다")
+    .color(TextColor.fromHexString("#FF5555")))
+
+val noHavePermission = TeamException(Component.text("해당 명령어를 사용할 수 있는 권한이 없습니다")
     .color(TextColor.fromHexString("#FF5555")))
--- src/main/kotlin/org/cheesesand/csTeam/commands/actions/op/FKickAction.kt
+++ src/main/kotlin/org/cheesesand/csTeam/commands/actions/op/FKickAction.kt
@@ -1,10 +1,15 @@
 package org.cheesesand.csTeam.commands.actions.op
 
 import org.bukkit.command.CommandSender
+import org.cheesesand.csTeam.*
 import org.cheesesand.csTeam.commands.actions.TeamActionCommand
 
 class FKickAction: TeamActionCommand(){
     override fun execute(sender: CommandSender, args: ArrayList<String>) {
+        if(!sender.isOp){
+            throw noHavePermission
+        }
+
         sender.sendMessage("FKickAction")
     }
 }
